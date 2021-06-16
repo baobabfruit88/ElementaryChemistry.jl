20545d8b48d5b7e1ab3187cf6b6a4c2646a93c7b
baobabfruit88@gmail.com <baobabfruit88@gmail.com>
2021-06-16 19:44:36 +0200

    Added docstrings

--- src/gasses.jl
+++ src/gasses.jl
@@ -4,9 +4,18 @@ temprature and/or pressure changes respectively :
 Charles and Boyles laws
 ```
 
-#When pressure is constant we can use charles law
-#Charles law states that volume expands or contracts 
-#Directly as absolute temprature changes when pressure is constant 
+"""
+When pressure is constant we can use charles law
+Charles law states that volume expands or contracts 
+Directly as temprature changes when pressure is constant 
+
+Calculate the new volume of a gas using Charles law 
+
+curentvolume = Currrent c.c of gas
+currentTemperature = The Current temprature in either C or F
+newTemperature = Temprature the gas is being heated, or cooled to
+tempratureUnit = C or F for Centigrade and Farenheit respectively.
+"""
 
 function charlesLaw(currentVolume,currentTemprature,newTemperature,tempratureUnit)
     newVolume = 0;
@@ -16,7 +25,11 @@ function charlesLaw(currentVolume,currentTemprature,newTemperature,tempratureUni
     return round(newVolume)
 end
 
-#We have to convert to absolute temprature first
+"""
+Converts farenheit or Centigrade to absolute temprature
+temp = temprature to Convert
+unit = F or C Farenheit or Centigrade respectively.
+"""
 function absoluteTemperature(temp,unit)
     if lowercase(unit) == "c"
         return round(temp + 273)
