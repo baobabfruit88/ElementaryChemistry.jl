1d12ef676ba34c19b3e9e7c7a6958a2f24a8fe77
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-06-17 16:05:56 +0530

    fixed typos

--- src/gasses.jl
+++ src/gasses.jl
@@ -1,20 +1,20 @@
 ```
-The gass laws to calculate volume by 
-temprature and/or pressure changes respectively : 
+The gas laws to calculate volume by
+temperature and/or pressure changes respectively :
 Charles and Boyles laws
 ```
 
 """
-When pressure is constant we can use charles law
-Charles law states that volume expands or contracts 
-Directly as temprature changes when pressure is constant 
+When pressure is constant we can use Charles law
+Charles law states that volume expands or contracts
+Directly as temperature changes when pressure is constant
 
-Calculate the new volume of a gas using Charles law 
+Calculate the new volume of a gas using Charles law
 
-curentvolume = Currrent c.c of gas
-currentTemperature = The Current temprature in either C or F
-newTemperature = Temprature the gas is being heated, or cooled to
-tempratureUnit = C or F for Centigrade and Farenheit respectively.
+curentVolume = Current c.c of gas
+currentTemperature = The Current temperature in either C or F
+newTemperature = Temperature the gas is being heated, or cooled to
+temperatureUnit = C or F for Centigrade and Fahrenheit respectively.
 """
 
 function charlesLaw(currentVolume,currentTemprature,newTemperature,tempratureUnit)
@@ -26,9 +26,9 @@ function charlesLaw(currentVolume,currentTemprature,newTemperature,tempratureUni
 end
 
 """
-Converts farenheit or Centigrade to absolute temprature
-temp = temprature to Convert
-unit = F or C Farenheit or Centigrade respectively.
+Converts Fahrenheit or Centigrade to absolute temperature
+temp = temperature to Convert
+unit = F or C Fahrenheit or Centigrade respectively.
 """
 function absoluteTemperature(temp,unit)
     if lowercase(unit) == "c"
@@ -37,4 +37,3 @@ function absoluteTemperature(temp,unit)
         return round((temp + 459) / 1.8)
     end
 end
-
