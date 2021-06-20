4724b0f099ace825cca2cd8b1671e29f07e70807
baobabfruit88@gmail.com <baobabfruit88@gmail.com>
2021-06-20 14:38:21 +0200

    Fixed myriad of typos

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 An elementary introduction into chemistry,
 Based on "An Elementary Chemistry" By Eric John Holmyard.
 
-Starting with Gass Laws : 
+Starting with Gas Laws : 
 
 - [x] Charles law 
 - [x] Boyles law
--- src/gasses.jl
+++ src/gasses.jl
@@ -1,27 +1,27 @@
 ```
-The gass laws to calculate volume by 
-temprature and/or pressure changes respectively : 
+The gas laws to calculate volume by 
+temperature and/or pressure changes respectively : 
 Charles, Boyles, Gay Lussacs and Avogadros laws
 ```
 
 """
 When pressure is constant we can use charles law
 Charles law states that volume expands or contracts 
-Directly as temprature changes when pressure is constant 
+Directly as temperature changes when pressure is constant 
 
 Calculate the new volume of a gas using Charles law 
 
-curentvolume = Currrent c.c of gas
-currentTemperature = The Current temprature in either C or F
-newTemperature = Temprature the gas is being heated, or cooled to
-tempratureUnit = (c)entigrade, (f)arenheit, or (a)bsolute
+curentVolume = Curent c.c of gas
+curenttemperature = The Curent temperature in either C or F
+newtemperature = temperature the gas is being heated, or cooled to
+temperatureUnit = (c)entigrade, (f)ahrenheit, or (a)bsolute
 """
 
-function charles_law(currentVolume,currentTemprature,newTemperature,tempratureUnit)
+function charles_law(curentVolume,curenttemperature,newtemperature,temperatureUnit)
     newVolume = 0;
-    currentTemprature = round(absoluteTemperature(currentTemprature,tempratureUnit))
-    newTemperature = round(absoluteTemperature(newTemperature,tempratureUnit))
-    newVolume = currentVolume * (newTemperature/currentTemprature)
+    curenttemperature = round(absolute_temperature(curenttemperature,temperatureUnit))
+    newtemperature = round(absolute_temperature(newtemperature,temperatureUnit))
+    newVolume = curentVolume * (newtemperature/curenttemperature)
     return round(newVolume)
 end
 
@@ -31,53 +31,53 @@ When temperature remains contstant but pressure changes we can calculate new vol
 Boyles law when solving for pressure. 
 Which simlpy states : New volume = old volume * old pressure devided by new pressure 
 
-currentPressure = The current pressure in mm
-currentVolume = The current volume of the gass
+curentPressure = The curent pressure in mm
+curentVolume = The curent volume of the gas
 desiredPressure = The pressure we want to be at
 """
-function boyles_law_solve_pressure(currentPressure, currentVolume,desiredPressure)
+function boyles_law_solve_pressure(curentPressure, curentVolume,desiredPressure)
     newVolume = 0;
-    newVolume = (currentVolume * currentPressure) / desiredPressure
+    newVolume = (curentVolume * curentPressure) / desiredPressure
     return newVolume
 end
 
 """
 When temperature remains contstant but volume changes we can calculate new pressure by using
 Boyles law when solving for volume. 
-Which simlpy states : New pressure = old pressure * current volume devided by new volume 
+Which simlpy states : New pressure = old pressure * curent volume devided by new volume 
 
-currentPressure = The current pressure in mm
-currentVolume = The current volume of the gass
+curentPressure = The curent pressure in mm
+curentVolume = The curent volume of the gas
 desiredVolume = The volume we want to have
 """
-function boyles_law_solve_volume(currentPressure,currentVolume,desiredVolume)
+function boyles_law_solve_volume(curentPressure,curentVolume,desiredVolume)
     newPressure = 0
-    newPressure = (currentPressure * currentVolume) / desiredVolume
+    newPressure = (curentPressure * curentVolume) / desiredVolume
     return newPressure
 end
 
 """
-The pressure of a gass of fixed mass and fixed volume is directly 
-proprtional to the gas's absolute temprature.
+The pressure of a gas of fixed mass and fixed volume is directly 
+proprtional to the gas's absolute temperature.
 
-If a gas's temprature increases, so does its pressure if the mass and volume are 
+If a gas's temperature increases, so does its pressure if the mass and volume are 
 held constant.
 
-pressure = Current pressure in atm
-temperature = Current temperature of container
-newTemp = Temperature at which we want to know the pressure
-tempratureUnit = (c)entigrade, (f)arenheit, or (a)bsolute
+pressure = Curent pressure in atm
+temperature = Curent temperature of container
+newTemp = temperature at which we want to know the pressure
+temperatureUnit = (c)entigrade, (f)ahrenheit, or (a)bsolute
 """
-function gay_lussacs_law(pressure, temperature, newTemp, tempratureUnit)
- newTemp = absoluteTemperature(newTemp,tempratureUnit)
- temperature = absoluteTemperature(temperature,tempratureUnit)
+function gay_lussacs_law(pressure, temperature, newTemp, temperatureUnit)
+ newTemp = absolute_temperature(newTemp,temperatureUnit)
+ temperature = absolute_temperature(temperature,temperatureUnit)
  newPressure = (pressure * newTemp) / temperature
  return newPressure
 end
 
 """
-Avrogados law under the assumption of ideal gasses teaches us that
-any gas, while having the same temprature, pressure and volume will have the 
+Avrogados law under the assumption of ideal gases teaches us that
+any gas, while having the same temperature, pressure and volume will have the 
 same number of molecules. or : V1|N1 = V2|N2
 """
 function avogadros_law()
@@ -85,17 +85,17 @@ function avogadros_law()
 end
 
 """
-Converts farenheit or Centigrade to absolute temprature
-temprature = temprature to Convert
-tempratureUnit = (c)entigrade, (f)arenheit, or (a)bsolute = (c)entigrade, (f)arenheit, or (a)bsolute
+Converts Fahreneit or Centigrade to absolute temperature
+temperature = temperature to Convert
+temperatureUnit = (c)entigrade, (f)ahrenheit, or (a)bsolute = (c)entigrade, (f)ahrenheit, or (a)bsolute
 """
-function absolute_temperature(temprature,tempratureUnit)
-    if lowercase(tempratureUnit) == "c"
-        return round(temprature + 273)
-    elseif lowercase(tempratureUnit) == "f"
-        return round((temprature + 459) / 1.8)
-    elseif lowercase(tempratureUnit) == "a"
-        return temprature    
+function absolute_temperature(temperature,temperatureUnit)
+    if lowercase(temperatureUnit) == "c"
+        return round(temperature + 273)
+    elseif lowercase(temperatureUnit) == "f"
+        return round((temperature + 459) / 1.8)
+    elseif lowercase(temperatureUnit) == "a"
+        return temperature    
     end
 end
 
