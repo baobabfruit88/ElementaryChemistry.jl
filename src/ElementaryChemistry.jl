797a50e91ee683a3f4c4c13ca632ccd1d32c96a7
baobabfruit <baobabfruit88@gmail.com>
2021-06-23 23:04:28 +0200

    Revert "Added grayhams law for velocity ratio between gasses a and b"
    This reverts commit 1401d57921aed26b9d6852146fc5b01957fe7de0.

--- src/ElementaryChemistry.jl
+++ src/ElementaryChemistry.jl
@@ -4,7 +4,6 @@ include("Gasses.jl")
 include("conversions/TempratureConversion.jl")
 include("conversions/PressureConversion.jl")
 
-export charles_law, boyles_law_solve_pressure, boyles_law_solve_volume, gay_lussacs_law, avogadros_law, absolute_temperature,
-grahams_law_velocity_ratio
+export charles_law, boyles_law_solve_pressure, boyles_law_solve_volume, gay_lussacs_law, avogadros_law, absolute_temperature
 
 end
--- src/Gasses.jl
+++ src/Gasses.jl
@@ -79,16 +79,4 @@ function avogadros_law()
     return null
 end
 
-"""
-One of the use cases of grahams law 
-is that we can calculate the ratio of diffusion rates of two gasses. 
 
-the rate of effusion or diffusion of a gass in inversely proprtional to the square
-root of the molar mass of the gas. 
-
-using g/mol give the molar masses of both gasses. 
-"""
-function grahams_law_velocity_ratio(molar_mass_a,molar_mass_b)
-        ratio = sqrt(molar_mass_a/molar_mass_b)
-    return round(ratio, digits = 3)
-end
