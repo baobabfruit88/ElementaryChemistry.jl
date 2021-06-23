1401d57921aed26b9d6852146fc5b01957fe7de0
baobabfruit <baobabfruit88@gmail.com>
2021-06-23 22:46:02 +0200

    Added grayhams law for velocity ratio between gasses a and b

--- src/ElementaryChemistry.jl
+++ src/ElementaryChemistry.jl
@@ -4,6 +4,7 @@ include("Gasses.jl")
 include("conversions/TempratureConversion.jl")
 include("conversions/PressureConversion.jl")
 
-export charles_law, boyles_law_solve_pressure, boyles_law_solve_volume, gay_lussacs_law, avogadros_law, absolute_temperature
+export charles_law, boyles_law_solve_pressure, boyles_law_solve_volume, gay_lussacs_law, avogadros_law, absolute_temperature,
+grahams_law_velocity_ratio
 
 end
